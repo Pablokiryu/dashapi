f73750c6fda8f30014e745e146d196c2b79069f4
Pablo Freitas Machado <Pablokiryu@hotmail.com>
2018-09-12 23:20:12 -0300

    Added .gitlab-ci.yml to integrate into GitLab CI/CD \n also fixed the Testsuit in Dashapi.test.js, no more errors, nor warnings... i had previously mixed up Promises and callbacks. .

--- tests/DashApi.test.js
+++ tests/DashApi.test.js
@@ -3,72 +3,54 @@ const request = require("supertest");
 const app = require("../app");
 
 
-// it("should add two numbers",() =>
-// {
-//     var res  = 2+3;
-//     res = res.toString();
-
-//     expect(res).toBe("5").toBeA("string");
-    
-// });
-
-// test("Adds 33 and 44", () =>{
-//     expect(33+44).toBe(77);
-// });
-
+///TODO : BREAK THIS INTO MULTIPLE TEST SUITES, decoupling routes and etc... 
+// should also provide better code coverage. This is barely enough. 
 describe("app testing", () => {
     it("has app module", () =>{ 
         expect(app).toBeDefined;
     });
 
     let server;
-    beforeAll(()=>{
-        server = app.listen(3000);
+    beforeAll((done)=>{
+        server = app.listen(3000,done);
     });
     
-    afterAll((done) =>{
+    afterAll( (done) =>{
         app.mongoose.connection.close();
-        server.close();
+        server.close(done);
+        process.exit();
     });
 
     describe("Users Route",()=>{
-        it("can create a new user", ()=>{
-            request(server).post("/Users").send({
+        it("can create a new user",async ()=>{
+            await request(server).post("/Users").send({
                 "name": "Sheev Palpatine"
             }).set('Content-Type', 'application/json')
             .set('Accept', 'application/json').expect(201);
         });
 
-        it("can fetch all Users", ()=>{
-            request(server).get("/Users").set('Accept', 'application/json').expect(200).end(function(err,res){
-                if(err) throw err;
-            });
+        it("can fetch all Users",async ()=>{
+            await request(server).get("/Users").set('Accept', 'application/json').expect(200);
         });
 
-        it("can fetch an specific User", ()=>{
+        it("can fetch an specific User", async()=>{
             var id = "5b908da8045cb4413c4a39be";
-            request(server).get(`/Users/${id}`).set('Accept', 'application/json').expect(200);
+            await request(server).get(`/Users/${id}`).set('Accept', 'application/json').expect(200);
         });
     });
     describe("topActiveUsers Route",()=>{
-        it("can fetch the top active Users", ()=>{
-            request(server).get("/topActiveUsers").set('Accept', 'application/json').expect(200).end(function(err,res){
-                if(err) throw err;
-            });
+        it("can fetch the top active Users",async ()=>{
+            await request(server).get("/topActiveUsers").set('Accept', 'application/json').expect(200);
         });
     });
-    describe("Applications Route",()=>{
-        it("can fetch all Applications", ()=>{
-            request(server).get("/Applications").set('Accept', 'application/json').expect(200).end(function(err,res){
-                if(err) throw err;
-            });
+    describe("Applications Route", ()=>{
+        it("can fetch all Applications",async ()=>{
+            await request(server).get("/Applications").set('Accept', 'application/json').expect(200);
         });
     });
     describe("Listings Route",()=>{
-        it("can fetch all Listings", ()=>{
-            request(server).get("/Listings").set('Accept', 'application/json').expect(200).end(function(err,res){
-                if(err) throw err;
-            });
+        it("can fetch all Listings",async ()=>{
+            await request(server).get("/Listings").set('Accept', 'application/json').expect(200);
         });
     });
 
